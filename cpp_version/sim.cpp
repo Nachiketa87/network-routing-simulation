dc3caceae34e01b271feff8d1e94defa7f0fa7c6
Nachiketa87 <keshu61bn@gmail.com>
2025-09-30 21:36:46 +0530

    backend logic fully completed along with the 20% rule in cpp

--- cpp_version/sim.cpp
+++ cpp_version/sim.cpp
@@ -30,11 +30,15 @@ vector<int> dijkstra(int src, int v, vector<vector<int>>& edges, vector<int>& pa
             }
         }
     }
+    cout << "dijsktra function executed" << endl;
     return dist;
 }
 
 vector<int> constructpath(int src, int dst, vector<int>& parent){
-    if(parent[dst] == -1) return {};
+    if(parent[dst] == -1){
+        cout << "constructpath: no parent found for dst " << dst << endl;
+        return {};
+    }
     int node = dst;
     vector<int> path = {dst};
     while(node != src){
@@ -43,17 +47,59 @@ vector<int> constructpath(int src, int dst, vector<int>& parent){
         path.push_back(node);
     }
     reverse(path.begin(), path.end());
+    cout << "constructpath function executed" << endl;
     return path;
 }
 
-void sim(int src, int dst, int v, vector<vector<vector<int>>>& edges, queue<pair<int,int>>& que, int hops, int prev, int total){
+bool deltalen(int prev_pathlen, int new_pathlen){
+    if(new_pathlen == 0) return true;
+    if(new_pathlen > 1.2 * prev_pathlen) return true;
+    if(new_pathlen < 0.8 * prev_pathlen) return true;
+    return false;
+}
+
+void sim(int src, int dst, int v, vector<vector<vector<int>>>& edges, queue<pair<int,int>>& que, int hops, int prev, 
+    int total, int prev_len, int prev_tab_ind, vector<int>& rem){
     int table = que.front().first;
     int trigger = que.front().second;
 
     vector<int> parent (v,-1);
     parent[src] = src;
     vector<int> dist = dijkstra(src,v,edges[table],parent);
-    vector<int> path = constructpath(src,dst,parent);
+    
+    int new_len = 0;
+    if(table > 0){
+        int n = edges[table].size(), cur = 0, nxt = 1; 
+        while(nxt < rem.size()){
+            for(int i=0; i<n; i++){
+                if(rem[cur] == edges[table][i][0] && rem[nxt] == edges[table][i][1] ||
+                rem[cur] == edges[table][i][1] && rem[nxt] == edges[table][i][0]){
+                    new_len += edges[table][i][2];
+                    cout << "new distance between node " << rem[cur] << " and node " << rem[nxt] << " is: " << edges[table][i][2] << endl;
+                    break;   
+                }
+            }
+            cur++; nxt++;
+        }
+    }
+    cout << "prev length of old path: " << prev_len << " and new length of old path: " << new_len << endl;
+    bool use_currtable = deltalen(prev_len,new_len);
+
+    cout << "use_currtable: " << use_currtable 
+     << " | table: " << table 
+     << " | prev_tab_ind: " << prev_tab_ind << endl;
+
+
+    vector<int> path; 
+    if(use_currtable){
+        prev_tab_ind = table;
+        path = constructpath(src,dst,parent);
+    }
+    else{
+        dist = dijkstra(src,v,edges[prev_tab_ind],parent);
+        path = constructpath(src,dst,parent);
+    }
+    cout << "prev_tab_ind value: " << prev_tab_ind << endl;
     if(path.empty()){
         cout << "No path exists between: " << src << " and " << dst << endl;
         return;
@@ -87,7 +133,18 @@ void sim(int src, int dst, int v, vector<vector<vector<int>>>& edges, queue<pair
         cout << "packet expired at: " << curr_pos;
         return;
     }
-    sim(curr_pos,dst,v,edges,que,hops,prev_pos,total);
+    int rem_pathlen = dist[dst] - dist[curr_pos];
+    cout << "rem_pathlen: " << rem_pathlen << " and index: " << indx << endl;
+
+    vector<int> new_rem; 
+    cout << "path's remaining nodes: ";
+    while(indx < path.size()){
+        new_rem.push_back(path[indx]);
+        cout << path[indx] << " ";
+        indx++;
+    }
+    cout << endl;
+    sim(curr_pos,dst,v,edges,que,hops,prev_pos,total,rem_pathlen,prev_tab_ind,new_rem);
 }
 
 int main(){
@@ -101,6 +158,7 @@ int main(){
     que.push({0,2});
     que.push({1,3});
     que.push({2,7});
+    vector<int> empty = {};
 
-    sim(src,dst,v,edges,que,0,src,0); 
+    sim(src,dst,v,edges,que,0,src,0,1e7,0,empty); 
 }
