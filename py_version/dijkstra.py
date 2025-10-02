5c38cd93662842fe653fe42c29ff4681e606ad31
Nachiketa87 <keshu61bn@gmail.com>
2025-10-02 18:59:34 +0530

    tracked events and paths in a dictionary for presentation file

--- py_version/dijkstra.py
+++ py_version/dijkstra.py
@@ -15,6 +15,8 @@ def dijkstra(src,v,edges,parent) -> list:
     while len(pq) > 0:
         dis,curr = heapq.heappop(pq)
         for nxt,nxtdis in adj[curr]:
+            if nxt == parent[curr]:
+                continue
             if dis + nxtdis < dist[nxt]:
                 dist[nxt] = dis + nxtdis
                 parent[nxt] = curr
@@ -44,7 +46,13 @@ def sim(src,v,dst,edges,que,hops,prev,total,prev_len=1e7,prev_tab_ind=0,rem=[]):
     parent = [-1] * v
     parent[src] = src
     dist = dijkstra(src,v,edges[table],parent)
-    
+
+    events = []
+    curr_path = []
+    if prev_len != 1e7:
+        curr_path.append(src)
+        events.append({"events":"Fluctuations detected, table updated", "table":table, "at_node":src})
+
     new_len = 0
     if(table > 0):
         n,cur,nxt = len(edges[table]),0,1
@@ -66,13 +74,16 @@ def sim(src,v,dst,edges,que,hops,prev,total,prev_len=1e7,prev_tab_ind=0,rem=[]):
         prev_tab_ind = table
         path = constructpath(src,dst,parent)
         cost = dist[dst]
+        if prev_len != 1e7:
+            events.append({"event":"Major fluctuations observed, new trajectory", "path":path})
     else:
         path = rem
         cost = new_len
+        events.append({"event":"Some minor fluctuations observed, resuming same path", "path":path})
 
     if not path:
-        print(f"No path exists between {src} and {dst}")
-        return
+        events.append({"event":"No path exists", "node":dst, "cost":None})
+        return {"path": None, "cost": None, "events": events}
     
     print(f"Decided path: {path} with projected cost: {cost}")
 
@@ -83,46 +94,59 @@ def sim(src,v,dst,edges,que,hops,prev,total,prev_len=1e7,prev_tab_ind=0,rem=[]):
             hops += 1
             curr_pos = path[indx]
             total += dist[curr_pos]
-            print(f"packet arrived at node: {curr_pos} with total cost at: {total}")
+            curr_path.append(curr_pos)
+            events.append({"event":"packet arrived", "node":curr_pos, "cost":total})
             continue
         if curr_pos == dst:
-            print(f"packet reach destination node: {dst} with total cost of {total}")
-            return
+            events.append({"event":"Arrived at destination", "node":dst, "cost":total})
+            return {"path":curr_path, "cost":total, "events":events}
+        
         indx += 1
         hops += 1
         prev_pos = curr_pos
         curr_pos = path[indx]
         total += dist[curr_pos] - dist[prev_pos]
-        print(f"packet arrived at node: {curr_pos} with total cost at: {total}")
+        curr_path.append(curr_pos)
+        events.append({"event":"packet arrived", "node":curr_pos, "cost":total})
     
     if curr_pos == dst:
-        print(f"packet reach destination node: {dst} with total cost of {total}")
-        return
+        events.append({"event":"Arrived at destination", "node":dst, "cost":total})
+        return {"path":curr_path, "cost":total, "events":events}
     if not que:
-        print(f"packet expired at node: {curr_pos}")
-        return
+        events.append({"event":"Packet expired","node":curr_pos,"cost":total})
+        return {"path":curr_path, "cost":total, "events":events}
+    
     rem_pathlen = dist[dst] - dist[curr_pos]
     new_rem = []
     while indx < len(path):
         new_rem.append(path[indx])
         indx += 1
     print("coming trajectory of packet: ",new_rem)
-    sim(curr_pos,v,dst,edges,que,hops,prev_pos,total,rem_pathlen,prev_tab_ind,new_rem)
+    res = sim(curr_pos,v,dst,edges,que,hops,prev_pos,total,rem_pathlen,prev_tab_ind,new_rem)
+   
+    all_events = events + res["events"]
+    all_paths = curr_path[:-1] + res["path"]
+    return {"path":all_paths, "cost":res["cost"], "events": all_events}
 
+if __name__ == "__main__":
+    
+    src,v,dst = 0,5,3
+    edges = [
+        [[0,1,4],[0,2,8],[1,4,6],[2,3,2],[3,4,10],[1,2,1]],
+        [[0,1,4],[0,2,15],[1,4,6],[2,3,30],[3,4,10],[1,2,1]],
+        [[0,1,4],[0,2,15],[1,4,12],[2,3,30],[3,4,5],[1,2,1]]
+    ] 
+    que = deque()
+    que.append((0,2))
+    que.append((1,3))
+    que.append((2,7))
 
-src,v,dst = 0,5,3
+    sol = sim(src,v,dst,edges,que,0,src,0)
 
-edges = [
-    [[0,1,4],[0,2,8],[1,4,6],[2,3,2],[3,4,10],[1,2,1]],
-    [[0,1,4],[0,2,15],[1,4,6],[2,3,30],[3,4,10],[1,2,1]],
-    [[0,1,4],[0,2,15],[1,4,12],[2,3,30],[3,4,5],[1,2,1]]
-] 
-que = deque()
-que.append((0,2))
-que.append((1,3))
-que.append((2,7))
-
-sim(src,v,dst,edges,que,0,src,0,1e7,0,[])
+    sol["path"].insert(0,src)
+    print("path traversed: ",sol["path"])
+    print("total cost observed: ",sol["cost"])
+    print("Events chronology: ",sol["events"])
 
 
 
